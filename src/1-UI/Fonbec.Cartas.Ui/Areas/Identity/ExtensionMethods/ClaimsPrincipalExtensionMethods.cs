0fcf0607d27afb12d3c3c93fb23a8561c17428f5
Pedro Ignacio Errico <Ignacio.Errico@gmail.com>
2023-05-29 10:01:33 -0300

    Display nickname from claim in LoginDisplay when user is logged in
    - Add extension methods to work with user's claims
      - To be honest, I'm not happy with the location of this new file; consider a moving all Identity related files that HAVE NOT BEEN scaffolded to another location

---
+++ src/1-UI/Fonbec.Cartas.Ui/Areas/Identity/ExtensionMethods/ClaimsPrincipalExtensionMethods.cs
@@ -0,0 +1,44 @@
+﻿using System.Security.Claims;
+
+namespace Fonbec.Cartas.Ui.Areas.Identity.ExtensionMethods
+{
+    public static class ClaimsPrincipalExtensionMethods
+    {
+        public static string NickName(this ClaimsPrincipal claimsPrincipal)
+        {
+            var nickName = claimsPrincipal.Claims
+                .FirstOrDefault(c => string.Equals(c.Type, FonbecUserCustomClaims.NickName, StringComparison.Ordinal));
+            
+            return nickName is null
+                ? "desconocido"
+                : nickName.Value;
+        }
+        
+        public static int? FilialId(this ClaimsPrincipal claimsPrincipal)
+        {
+            var filialIdString = claimsPrincipal.Claims
+                .FirstOrDefault(c => string.Equals(c.Type, FonbecUserCustomClaims.FilialId, StringComparison.Ordinal));
+            
+            if (filialIdString is null)
+            {
+                return null;
+            }
+
+            if (!int.TryParse(filialIdString.Value, out var filialId))
+            {
+                return null;
+            }
+
+            return filialId;
+        }
+
+        public static bool IsAdmin(this ClaimsPrincipal claimsPrincipal)
+        {
+            var nameIdentifier = claimsPrincipal.Claims
+                .FirstOrDefault(c => string.Equals(c.Type, ClaimTypes.NameIdentifier, StringComparison.Ordinal));
+            
+            return nameIdentifier is not null
+                   && string.Equals(nameIdentifier.Value, "admin", StringComparison.Ordinal);
+        }
+    }
+}
