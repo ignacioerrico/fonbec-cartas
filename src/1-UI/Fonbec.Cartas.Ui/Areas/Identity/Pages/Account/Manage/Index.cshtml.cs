2aa24d4a857b2413177f48ce9a9d7c3a27e21034
Pedro Ignacio Errico <Ignacio.Errico@gmail.com>
2023-06-25 18:38:15 -0300

    Translate Identity/Account/Manage pages
    - Plus minor improvements

--- src/1-UI/Fonbec.Cartas.Ui/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ src/1-UI/Fonbec.Cartas.Ui/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -30,6 +30,7 @@ namespace Fonbec.Cartas.Areas.Identity.Pages.Account.Manage
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
+        [Display(Name = "Usuario")]
         public string Username { get; set; }
 
         /// <summary>
@@ -57,7 +58,7 @@ namespace Fonbec.Cartas.Areas.Identity.Pages.Account.Manage
             ///     directly from your code. This API may change or be removed in future releases.
             /// </summary>
             [Phone]
-            [Display(Name = "Phone number")]
+            [Display(Name = "Número de teléfono")]
             public string PhoneNumber { get; set; }
         }
 
